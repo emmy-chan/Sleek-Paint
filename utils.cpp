2da8867eb3267d15c99de8fb198baadad309c8e8
EmmyPoo <xkr0wnxyt@gmail.com>
2024-07-19 22:54:36 -0700

    removed non used func

--- utils.cpp
+++ utils.cpp
@@ -48,38 +48,6 @@ bool cUtils::MouseReleased(const uint8_t& btn)
 	return !ImGui::GetIO().MouseDown[btn] && ImGui::GetIO().MouseDownDurationPrev[btn] > 0.f;
 }
 
-//This will also adapt to our camera position
-// Fix coordinates according to our canvas [Credits: Javidx9]
-glm::u32vec2 cUtils::MapCoordsToRect(glm::u32vec2 coord, const int& w, const int& h) {
-    auto& io = ImGui::GetIO();
-
-    glm::u16vec2 vViewPos;
-    glm::u16vec2 vViewSize;
-    glm::u16vec2 vWindowSize = { w, h };
-    const float wasp = (float)(w) / (float)(h);
-
-    vViewSize.x = (int32_t)vWindowSize.x;
-    vViewSize.y = (int32_t)((float)vViewSize.x / wasp);
-
-    if (vViewSize.y > vWindowSize.y)
-    {
-        vViewSize.y = vWindowSize.y;
-        vViewSize.x = (int32_t)((float)vViewSize.y * wasp);
-    }
-
-    vViewPos = { (w - vViewSize.x) / 2, (h - vViewSize.y) / 2 };
-
-    coord.x -= vViewPos.x + (int)g_cam.x;
-    coord.y -= vViewPos.y + (int)g_cam.y;
-    coord.x = (int32_t)(((float)coord.x / (float)(vWindowSize.x - (vViewPos.x * 2)) * (float)(g_canvas[g_cidx].width * TILE_SIZE)));
-    coord.y = (int32_t)(((float)coord.y / (float)(vWindowSize.y - (vViewPos.y * 2)) * (float)(g_canvas[g_cidx].height * TILE_SIZE)));
-
-    //coord.x = glm::clamp<int>(mouse.x, 0, w);
-    //coord.y = glm::clamp<int>(mouse.y, 0, h);
-
-    return coord;
-}
-
 // Function to calculate the squared distance between two colors to avoid taking the square root unnecessarily
 int cUtils::ColorDistanceSquared(const ImU32& col1, const ImU32& col2) {
     const int r1 = (col1 >> IM_COL32_R_SHIFT) & 0xFF;
